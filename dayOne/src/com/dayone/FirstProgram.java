75c5ab67c0da86d81fc9a9bc8f0fc6d25478b4e5
HP <HP@LAPTOP-IQH98KT3>
2025-06-30 11:44:20 +0530

    dayone

---
+++ dayOne/src/com/dayone/FirstProgram.java
@@ -0,0 +1,16 @@
+package com.dayone;
+
+public class FirstProgram {
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World" + " " + 12);
+		
+		System.out.println("Java Programming");
+		
+		System.out.print(234 + 10);
+	}
+	
+	
+
+}
---
+++ dayOne/src/com/dayone/primitivedatatypes/PrimitiveDatatypes.java
@@ -0,0 +1,55 @@
+package com.dayone.primitivedatatypes;
+
+public class PrimitiveDatatypes {
+
+	public static void main(String[] args)
+	{
+		// declaration
+		int x;
+		
+		//initializatio
+		x=67;
+		
+		System.out.println(x);
+		
+		//declaration and initialization
+		//byte
+		byte b = 10;
+		System.out.println(b);
+		
+		// short
+		short s = 4567;
+		System.out.println(s);
+		
+		// int
+		int a = 223456789;
+		System.out.println(a);
+		
+		//long
+		long l = 987766892345678923L;
+		System.out.println(l);
+		
+		//char 
+		char c = 'a';
+		System.out.println(c);
+		
+		char ch = '1';
+		System.out.println(ch);
+		
+		//float
+		float f = 3435.4566f;
+		System.out.println(f);
+		
+		//double 
+		double d = 3423.4566d;
+		System.out.println(d);
+		
+		//boolean
+		
+		boolean bool= true;
+		System.out.println(bool);
+		
+		
+		
+	}
+}
