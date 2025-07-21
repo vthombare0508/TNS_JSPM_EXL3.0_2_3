7e51626d100896c5dd1f2b8c11f85f2ea5668f93
HP <HP@LAPTOP-IQH98KT3>
2025-07-21 10:32:49 +0530

    Interview coding questions

---
+++ CodingQuestion/src/com/codingquestions/PalindromicString.java
@@ -0,0 +1,38 @@
+package com.codingquestions;
+
+public class PalindromicString {
+
+	 public static String smallestPalindromicString(int K) {
+	        StringBuilder result = new StringBuilder();
+	        char currentChar = 'a';
+
+	        while (K > 0) {
+	            int x = 1;
+
+	            // Find the largest x such that x*(x+1)/2 <= K
+	            while ((x + 1) * (x + 2) / 2 <= K) {
+	                x++;       
+	            }
+
+	            // Append currentChar x times
+	            for (int i = 0; i < x; i++) {
+	                result.append(currentChar);    
+	            }
+
+	            // Subtract the number of palindromic substrings contributed by this sequence
+	            K -= x * (x + 1) / 2;      
+
+	            // Move to the next lexicographically larger character
+	            currentChar++;    
+	        }
+
+	        return result.toString();  
+	    }
+
+	    public static void main(String[] args) {
+	        int K = 5;  // Change this value to test different inputs
+	        String ans = smallestPalindromicString(K);
+	        System.out.println("Lexicographically smallest string with " + K + " palindromic substrings: " + ans);
+	    }
+
+}
